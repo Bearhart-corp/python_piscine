19291f73261681ea8dad57f06f64b86352c47f28
Logosh <101455421+Bearhart-corp@users.noreply.github.com>
2026-01-30 17:29:46 +0100

    gen x garden and y plants

--- module1/ex6/ft_garden_analytics.py
+++ module1/ex6/ft_garden_analytics.py
@@ -1,13 +1,16 @@
 class GardenManager:
+
     def __init__(self):
+        self.garden_stat = self.GardenStats
         self.gardens = []
-        # ou ss init et 1 seul manager avec une liste de jardin
 
-    def add_garden(self, garden_name):
-        self.gardens.append(garden_name)
+    def add_garden(self, name: str):
+        garden = Garden(name)
+        self.gardens.append(garden)
+        return garden
 
-    def create_garden_network(cls):
-        pass
+    def create_garden_network(self):
+        return self.gardens
 
     @staticmethod
     def validate_height(height):  # quimporte l'instance donnee
@@ -18,27 +21,73 @@ class GardenManager:
             self.garden = garden
 
         def count_plants(self):
-            pass
+            return len(self.garden.plants)
 
         def total_growth(self):
-            pass
+            total = 0
+            for plant in self.garden.plants:
+                total += self.garden.plants.height
+            return total
 
         def type_breakdown(self):
-            pass
+            res = {}
+            for plant in self.garden.plants:
+                t = type(plant).__name__
+                res[t] = res.get(t, 0) + 1
+            return res
 
 
 class Garden:
 
-    def __init__(self, name, capacity):
+    def __init__(self, name: str):
         self.name = name
-        self.capacity = capacity
         self.plants = []
 
-    def add_plant(self, plant):
-        if len(self.plants) >= self.capacity:
+    @staticmethod
+    def rand_maison(seed, even: int) -> int:
+        i = even
+        j = id(i) % 0x100
+        even = not (even & 1)
+        seed = (((seed * 13 + i + 37 + even) ^ (0xa5 * even + i) ^ j) >> 1)
+        seed = ((seed ^ 0x5555 + 0x1111 - i + even) ^ 0x3333 + i) ^ j
+        return seed % 200
+
+    @staticmethod
+    def is_unique(seen, i: int, seed: int, seed2: int) -> int:
+        for j in range(0, i):
+            if seed == seen[j][0] and seed2 == seen[j][1]:
+                return False
+        return True
+
+    def gen_plants(self, n: int, input_seed: int) -> None:
+        if n > 400:
             return print("error")
-        else:
-            self.plants.append(plant)
+        plants = []
+        seen = [[0] * 2 for _ in range(400)]
+        i = 0
+        seed = input_seed
+        seed2 = input_seed << 2 ^ 0x55
+        names = ["Acer", "Salvia", "Verbena", "Laurus", "Rubus", "Ulmus",
+                 "Juniperus", "Ficus", "Plantago", "Artemisia", "Quercus",
+                 "Betula", "Alnus", "Populus", "Fraxinus", "Malva",
+                 "Ruta", "Mentha", "Thymus", "Cistus"]
+        len_names = len(names)
+
+        for i in range(n):
+            seed = Garden.rand_maison(seed, i)
+            seed2 = Garden.rand_maison(seed2, i)
+            while not Garden.is_unique(seen, i, seed % len_names,
+                                       seed2 % len_names):
+                seed = Garden.rand_maison(seed, i)
+                seed2 = Garden.rand_maison(seed2, i)
+            seen[i][0] = (seed % len_names)
+            seen[i][1] = (seed2 % len_names)
+            name = names[seed % len_names] + " " + names[seed2 % len_names]
+            plants.append(Plant(name, seed, seed2))
+        return plants
+
+    def add_plant(self, plant):
+        self.plants.append(plant)
 
     def total_water_need(self):
         count = 0
@@ -46,12 +95,12 @@ class Garden:
             count += plant.water_need()
         return count
 
-    def grow_all(self, plant):
+    def grow_all(self):
         for p in self.plants:
-            self.plants[p].age += 1
+            p.age += 1
 
-    def stats(self, GardenStats):
-        pass
+    def stats(self):
+        return GardenManager.GardenStats(self)
 
 
 class Plant:
@@ -76,86 +125,30 @@ class FloweringPlant(Plant):
 
 class PrizeFlower(FloweringPlant):
     def points(self):
-        pass
+        return id(self.color) % 20
 
 
-class PlantFactory:
-    @staticmethod
-    def rand_maison(seed, even: int) -> int:
-        i = even
-        j = id(i) % 0x100
-        even = not (even & 1)
-        seed = (((seed * 13 + i + 37 + even) ^ (0xa5 * even + i) ^ j) >> 1)
-        seed = ((seed ^ 0x5555 + 0x1111 - i + even) ^ 0x3333 + i) ^ j
-        return seed % 200
+def main(nbr_garden: int, nbr_plants: list):
+    manager = GardenManager()
+    dico = []
+    plants = []
+    k = 0
+    for i in range(0, 10):
+        dico.append(f"test{i}")
 
-    @staticmethod
-    def is_unique(seen, i: int, seed: int, seed2: int) -> int:
-        for j in range(0, i):
-            if seed == seen[j][0] and seed2 == seen[j][1]:
-                return False
-        return True
+    for j in range(0, nbr_garden):
+        garden = manager.add_garden(f"Garden{j}")
 
-    @staticmethod
-    def gen_plants(n: int, input_seed: int) -> None:
-        plants = {}
-        if n > 400:
-            return print("error")
-        seen = [[0] * 2 for _ in range(400)]
-        i = 0
-        seed = input_seed
-        seed2 = input_seed << 2 ^ 0x55
-        names = ["Acer", "Salvia", "Verbena", "Laurus", "Rubus", "Ulmus",
-                 "Juniperus", "Ficus", "Plantago", "Artemisia", "Quercus",
-                 "Betula", "Alnus", "Populus", "Fraxinus", "Malva",
-                 "Ruta", "Mentha", "Thymus", "Cistus"]
-        len_names = len(names)
-
-        for i in range(0, n):
-            seed = rand_maison(seed, i)
-            seed2 = rand_maison(seed2, i)
-            while not is_unique(seen, i, seed % len_names, seed2 % len_names):
-                seed = rand_maison(seed, i)
-                seed2 = rand_maison(seed2, i)
-            seen[i][0] = (seed % len_names)
-            seen[i][1] = (seed2 % len_names)
-            name = names[seed % len_names] + " " + names[seed2 % len_names]
-            plants[name] = Plant(name, seed, seed2)
-            plants[name].display(plants[name].age)
+    for garden in manager.gardens:
+        plants = garden.gen_plants(nbr_plants[k], id(dico[k]))
+        k += 1
+        print(f"\n{garden.name}\n")
+        for plant in plants:
+            garden.add_plant(plant)
+            plant.display()
+        stats = GardenManager.GardenStats(garden)
+        print(f"\n{stats.count_plants()}")
 
 
 if __name__ == "__main__":
-    dico = []
-    for i in range(0, 10):
-        dico.append(i)
-
-    for j in range(0, 10):
-        gen_plants(10, id(dico[j]))
-        print(f"\n === garden number: {j}\n\n")
-
-
-# GardenManager
-#  ├── gardens (dict)
-#  ├── add_garden()            → self
-#  ├── create_garden_network() → cls
-#  ├── validate_height()       → staticmethod
-#  └── GardenStats (nested)
-#        ├── count_plants()
-#        ├── total_growth()
-#        └── type_breakdown()
-
-# Garden
-#  ├── plants (list)
-#  ├── add_plant()             → self
-#  ├── grow_all()              → self
-#  └── stats                   → GardenStats
-
-# Plant
-#  ├── grow()                  → self
-
-# FloweringPlant(Plant)
-#  ├── color
-#  ├── blooming
-
-# PrizeFlower(FloweringPlant)
-#  ├── points
+    main(3, [4, 3, 20])
